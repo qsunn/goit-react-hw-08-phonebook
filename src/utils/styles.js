17da24d3d067b9f5b2eb74bac18a4dcbb1785ce3
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-03 17:35:02 +0200

    finished

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,7 +1,7 @@
 export const styles = {
     activeLink: ({ isActive }) => (isActive ? 'text-lime-300' : 'white'),
     container: 'flex flex-col gap-4 justify-center mx-auto md:w-80',
-    button: 'px-8 py-2 bg-lime-400 text-zinc-900 font-medium uppercase rounded-md',
+    button: 'px-8 py-2 bg-lime-400 text-zinc-900 font-medium uppercase rounded-md hover:text-white hover:bg-lime-400',
     title: 'font-bold text-xl text-center',
     flexRow: 'flex gap-4',
     flexCol: 'flex flex-col gap-4',
