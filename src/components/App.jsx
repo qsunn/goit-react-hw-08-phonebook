bce45e520c488ba257cb92d69cd7e66c53202ef7
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-03 17:03:43 +0200

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
 import { lazy } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/operations';
+import { selectAuth } from 'redux/selectors';
 import { Layout } from './Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
@@ -10,6 +14,14 @@ const RegisterPage = lazy(() => import('pages/RegisterPage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useSelector(selectAuth);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  if (isRefreshing) return <div className='text-center'>Refreshing user...</div>
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
